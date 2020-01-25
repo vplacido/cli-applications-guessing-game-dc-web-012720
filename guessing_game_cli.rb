e424d9682aac1a92a068ab9ee8f67c954f7f4e16
Valentin Placido <valentinplacido25@gmail.com>
2020-01-25 03:21:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
+    break
   else 
     puts "Sorry! The computer guessed #{randomNum}."
   end
